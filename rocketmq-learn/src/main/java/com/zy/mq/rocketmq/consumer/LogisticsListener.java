33773df040cbb61f172411eedaaae626f19e4d97
ZhangYDevelop <2389311433@qq.com>
2020-12-09 13:28:59 +0800

    整理项目结构

--- rocketmq-learn/src/main/java/com/zy/mq/rocketmq/consumer/LogisticsListener.java
+++ rocketmq-learn/src/main/java/com/zy/mq/rocketmq/consumer/LogisticsListener.java
@@ -2,7 +2,7 @@ package com.zy.mq.rocketmq.consumer;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zy.mq.rocketmq.dto.OrderDTO;
-import com.zy.mq.rocketmq.service.SmsService;
+import com.zy.mq.rocketmq.service.LogisticsService;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.slf4j.Logger;
@@ -11,18 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RocketMQMessageListener(consumerGroup = "pay_order_group",topic = "logistics_tx_topic")
-public class SmsListenerLocal implements RocketMQListener<String> {
+@RocketMQMessageListener(consumerGroup = "logistics_group",topic = "pay_order_tx_topic")
+public class LogisticsListener implements RocketMQListener<String> {
 
-    private Logger logger = LoggerFactory.getLogger(SmsListenerLocal.class);
+    private Logger logger = LoggerFactory.getLogger(LogisticsListener.class);
 
     @Autowired
-    private SmsService smsService;
+    private LogisticsService logisticsService;
 
     @Override
     public void onMessage(String message) {
         OrderDTO orderDto = JSONObject.parseObject(message, OrderDTO.class);
-        logger.info("开始发起物流订单： {}", orderDto);
-        smsService.sentSms(orderDto.getOrderId());
+        logger.info("发起物流订单： {}", message);
+        logisticsService.createLogistics(orderDto.getOrderId());
     }
 }
--- rocketmq-learn/src/main/java/com/zy/mq/rocketmq/service/impl/LogisticsServiceImpl.java
+++ rocketmq-learn/src/main/java/com/zy/mq/rocketmq/service/impl/LogisticsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.zy.mq.rocketmq.service.impl;
 
 import com.zy.mq.rocketmq.service.LogisticsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LogisticsServiceImpl implements LogisticsService {
     @Override
     public void createLogistics(String id) {
